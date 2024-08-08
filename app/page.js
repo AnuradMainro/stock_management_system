02efab0978f20701b53d3791f55bb87072385596
Noorinder Singh <noorindersingh@edu.sait.ca>
2024-08-08 17:42:51 -0600

    updated categories
    added the line for stock reports

--- app/page.js
+++ app/page.js
@@ -1,3 +1,47 @@
+// "use client";
+
+// import React, { useState } from "react";
+// import { AuthContextProvider } from "./_utils/auth-context"; // Adjust the path as necessary
+// import Login from "./components/Login";
+// import Dashboard from "./dashboard";
+// import ScanAndManage from "./scan_and_manage";
+// import StockInsertion from "./stock_insertion";
+// import StockReports from "./pages/stock-reports"; // Ensure this is correctly imported
+
+// function Page() {
+//   const [view, setView] = useState("login");
+
+//   const login = (user) => {
+//     if (user) {
+//       setView("dashboard");
+//     } else {
+//       alert("Login failed. Please check your credentials.");
+//     }
+//   };
+
+//   const navigateTo = (page) => {
+//     setView(page);
+//   };
+
+//    return (
+//     <AuthContextProvider>
+//       {view === "login" ? (
+//         <Login onLogin={login} />
+//       ) : view === "dashboard" ? (
+//         <Dashboard
+//           onNavigate={navigateTo}
+//           onLogout={() => navigateTo("login")}
+//         />
+//       ) : view === "scanAndManage" ? (
+//         <ScanAndManage onBack={() => navigateTo("dashboard")} />
+//       ) : view === "stockInsertion" ? (
+//         <StockInsertion onBack={() => navigateTo("dashboard")} />
+//       ) : null}
+//     </AuthContextProvider>
+//   );
+// }
+
+// export default Page;
 "use client";
 
 import React, { useState } from "react";
@@ -6,7 +50,7 @@ import Login from "./components/Login";
 import Dashboard from "./dashboard";
 import ScanAndManage from "./scan_and_manage";
 import StockInsertion from "./stock_insertion";
-import StockReports from "./pages/stock-reports"; // Ensure this is correctly imported
+import StockReports from "./pages/stock-reports"; // Ensure this path is correct
 
 function Page() {
   const [view, setView] = useState("login");
@@ -23,7 +67,7 @@ function Page() {
     setView(page);
   };
 
-   return (
+  return (
     <AuthContextProvider>
       {view === "login" ? (
         <Login onLogin={login} />
@@ -36,6 +80,8 @@ function Page() {
         <ScanAndManage onBack={() => navigateTo("dashboard")} />
       ) : view === "stockInsertion" ? (
         <StockInsertion onBack={() => navigateTo("dashboard")} />
+      ) : view === "stockReports" ? ( // Handle navigation to StockReports
+        <StockReports onBack={() => navigateTo("dashboard")} />
       ) : null}
     </AuthContextProvider>
   );
