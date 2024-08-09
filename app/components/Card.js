c9ae717c2bcd7eca0d1c48f18cb6f8e48842d61a
AnuradMainro <123777065+AnuradMainro@users.noreply.github.com>
2024-08-09 04:18:34 -0600

    updates

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,13 +1,13 @@
 // components/Card.js
 function Card({ title, onClick }) {
     return (
-      <div className="bg-yellow-600 p-8 rounded-xl shadow-lg hover:shadow-2xl transition duration-300 ease-in-out cursor-pointer" onClick={onClick}>
-        <h2 className="text-lg text-white">{title}</h2>
+      <div className="bg-[#ca8a04] p-8 rounded-xl shadow-lg h-[200px] hover:shadow-2xl transition duration-300 ease-in-out cursor-pointer" onClick={onClick}>
+        <h2 className="text-lg font-bold text-white flex items-center h-full">{title}</h2>
       </div>
     );
   }
   
   export default Card;
+
   
-  // This is a test by noor
   
